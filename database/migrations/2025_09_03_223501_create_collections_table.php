33cd366367cbcdea3490f13e65ae9ed6f7cee462
Gabrielrez <gabrielrezcpessoa@gmail.com>
2025-09-03 19:41:30 -0300

    feat: collections, goals and user_collection migrations created

---
+++ database/migrations/2025_09_03_223501_create_collections_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->max(45);
+            $table->text('description')->nullable()->max(255);
+            $table->boolean('cyclic')->default(false);
+            $table->date('deadline')->nullable();
+            $table->boolean('is_collaborative')->default(false);
+            $table->string('status')->default('in_progress');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collections');
+    }
+};
